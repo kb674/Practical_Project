2718d3ad7d4076c09c99876f41109545541bc003
kb674 <kushalbhikhabhai@gmail.com>
2021-05-31 14:57:59 +0000

    Added test

---
+++ service_two_api/tests/test_two.py
@@ -0,0 +1,38 @@
+from flask import url_for
+from flask_testing import TestCase
+from app import app
+from unittest.mock import patch 
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestHome(TestBase):
+    def test_get_name(self):
+        response = self.client.get(url_for('get_name'))
+        self.assertEqual(response.status_code, 200)
+
+        with patch ('random.choice') as r:
+            r.return_value = 'Shoshana'
+
+        response.data = r.return_value
+        self.assertIn(b'Shoshana', response.data)
+
+
+    def test_get_martial_art(self):
+        response = self.client.get(url_for('get_martial_art'))
+        self.assertEqual(response.status_code, 200)
+
+        with patch ('random.choice') as r:
+            r.return_value = 'Wrestling'
+
+        response.data = r.return_value
+        self.assertIn(b'Wrestling', response.data)
+
+       
+        
+        
+
+    
+        
+    
