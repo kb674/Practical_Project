4fd19d300169b97634ed934048b0667dea03aae9
Kushal Bhikhabhai <81389084+kb674@users.noreply.github.com>
2021-05-31 22:05:18 +0100

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,7 +3,7 @@
 echo "push script run"
 
 # login into dockerhub
-docker login -u $DOCKER_USER_NAME -p password $DOCKER_PASSWORD
+docker login -u $DOCKERHUB_DETAILS_USR -p password $DOCKERHUB_DETAILS_PSW
 
 # push up kb674/fighter_server_image:latest
 docker push kb674/fighter_two_image:latest
