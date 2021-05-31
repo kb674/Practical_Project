08614071762abfaaa4525a1df64b6ee2c8cf5454
Kushal Bhikhabhai <81389084+kb674@users.noreply.github.com>
2021-05-31 21:22:33 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 echo "build script run"
 
 #build server image called: kb674/fighter_server_image:latest
-docker build -t kb674/fighter_server_image:latest /server
+docker build -t kb674/fighter_server_image:latest server
