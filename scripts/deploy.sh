cfa0a70842843c870bec68a7be0edbe06f1e2dae
Kushal Bhikhabhai <81389084+kb674@users.noreply.github.com>
2021-06-03 10:26:11 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,11 @@
 echo "deploy script run"
 
 # send docker-compose file to swarm-manager
-scp docker-compose.yaml project-swarm-manager
+# scp docker-compose.yaml project-swarm-manager
 
 # ssh heredoc command
-ssh kusha@project-swarm-manager << EOF
-docker stack deploy --compose-file docker-compose.yaml service
-EOF
+#ssh kusha@project-swarm-manager << EOF
+#docker stack deploy --compose-file docker-compose.yaml service
+#EOF
 
 
