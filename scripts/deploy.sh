64c57e08ee6435c84e8d2de6b951aa9da88e1d43
Kushal Bhikhabhai <81389084+kb674@users.noreply.github.com>
2021-06-03 11:41:22 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,5 +14,11 @@ cd /home/jenkins
 docker stack deploy --compose-file docker-compose.yaml service 
 EOF
 
+# run nginx container
+ssh jenkins@project-nginx << EOF
+cd /home/jenkins
+docker run -d -p 80:80 --name nginx --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
+EOF
+
 
 
