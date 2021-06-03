27f00bc5184c062eed8aca3d020cc7a883ea6af3
Kushal Bhikhabhai <81389084+kb674@users.noreply.github.com>
2021-06-03 11:19:25 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,14 @@
 
 echo "deploy script run"
 
+# Send docker-compose file to sawrm manager
+scp docker-compose.yaml jenkins@project-swarm-manager:/home/jenkins/
+
+# change to jenkins directory run the deploy command
+ssh jenkins@project-swarm-manager << EOF 
+cd /home/jenkins
+docker stack deploy --compose-file docker-compose.yaml service 
+EOF
 
 
 
