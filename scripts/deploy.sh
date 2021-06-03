a26fcc7d38f35788f9740db2c7a8a4e71d582c02
Kushal Bhikhabhai <81389084+kb674@users.noreply.github.com>
2021-06-03 11:45:12 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,6 @@ scp nginx.conf jenkins@project-nginx:/home/jenkins/
 # run nginx container
 ssh jenkins@project-nginx << EOF
 cd /home/jenkins
-docker run -d -p 80:80 --name nginx --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
+docker run -d -p 80:80 --name nginx --mount type=bind,source=/home/jenkins/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
 EOF
 
